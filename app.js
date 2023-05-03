fb3c800fda6851d70ad04b816205ee6ae574dc25
nturk <nart_turk@bmc.com>
2023-05-03 19:00:10 +0300

    continue working on project

--- app.js
+++ app.js
@@ -10,23 +10,40 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res){
+  res.render("home", {StartingContent: homeStartingContent, posts: posts})
+})
 
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent})
+})
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent})
+})
 
+app.get("/compose", function(req, res){
+  res.render("compose")
+})
 
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
 
-
-
-
-
-
+  posts.push(post)
+  res.redirect("/")
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
